f4a2e9a245b7e489328bd020582aa3884fdd4447
Javier Sanchez <javilondo56@gmail.com>
2023-02-02 10:15:14 +0100

    navbar

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,7 +2,7 @@ import { HomePageWrapper } from "@/components/home/Home.styled";
 import HomeDescription from "@/components/home/HomeDescription/HomeDescriptrion";
 import HomePhoto from "@/components/home/HomePhoto/HomePhoto";
 import HomeTags from "@/components/home/HomeTags/HomeTags";
-import Navbar from "@/components/NavBar/NavBar";
+import Navbar from "@/components/NavBar/Navbar";
 
 const HomePage = () => {
     return (
